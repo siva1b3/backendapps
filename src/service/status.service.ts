4f2af847393a161f805703e8540a135a379fe07d
SivaDev <42565813+siva1b3@users.noreply.github.com>
2025-05-14 20:03:46 +0530

    jj

--- src/service/status.service.ts
+++ src/service/status.service.ts
@@ -1,14 +1,19 @@
 import prisma from "../PrismaComponet/Prisma.js";
+import { stc_status } from "../../generated/client.js";
 
-const getStatusList = async () => {
+const getStatusList = async (): Promise<stc_status[]> => {
   try {
-    const statusList = await prisma.stc_status.findMany();
-    if (!statusList) {
+    const statusList: stc_status[] = await prisma.stc_status.findMany();
+    if (!statusList || statusList.length === 0) {
       throw new Error("No status found");
     }
     return statusList;
-  } catch (error) {
-    throw new Error("Error fetching status list");
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(`Error fetching status list: ${error.message}`);
+    } else {
+      throw new Error("Unknown error fetching status list");
+    }
   }
 };
 
