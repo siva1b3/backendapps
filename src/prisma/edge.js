8b4bfd2f79c3c958d881bf75dacfac3619df94af
SivaDev <42565813+siva1b3@users.noreply.github.com>
2025-06-18 21:43:50 +0530

    test 3

--- src/prisma/edge.js
+++ src/prisma/edge.js
@@ -209,7 +209,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/prisma/index.js
+++ src/prisma/index.js
@@ -210,7 +210,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
