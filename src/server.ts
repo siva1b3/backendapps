ce6110a327eb6872b5cc95925cc7e452292aaf7d
SivaDev <42565813+siva1b3@users.noreply.github.com>
2025-06-18 21:29:03 +0530

    test 1

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import app from "./app.js";
+import { Request, Response } from "express";
 
-import prisma from "./prismaClient/Prisma.js";
+app.get("/", (_: Request, res: Response) => {
+  res.send("Hello, World!");
+});
 
 app.listen(3000, async () => {
-  const result = await prisma.a0_task.findMany();
-  console.log("Fetched tasks:", result);
   console.log("Server is running on http://localhost:3000");
 });
